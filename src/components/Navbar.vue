cdc5c58fd3f32d79c9037215110af559f00eb527
zkunsang <zkunsang@gmail.com>
2019-09-01 23:24:43 +0900

    [] - 뷰티파이 테스트

---
+++ src/components/Navbar.vue
@@ -0,0 +1,45 @@
+<template>
+    <nav>
+        <v-toolbar flat app>
+            <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+            <v-toolbar-title class="text-uppercase grey--text">
+                <span class="font-weight-light">Todo</span>
+                <span>Ninja</span>
+            </v-toolbar-title>
+            <v-spacer></v-spacer>
+            <v-btn text color="grey">
+                <span>Sign Out</span>
+                <v-icon right>mdi-exit-to-app</v-icon>
+            </v-btn>
+        </v-toolbar>
+
+        <v-navigation-drawer v-model="drawer" app class="indigo">
+            <v-list>
+                <v-list-item class="white--text" v-for="link in links" :key="link.icon" router :to="link.route">
+                    <v-list-item-icon>
+                        <v-icon>{{link.icon}}</v-icon>
+                    </v-list-item-icon>
+                    <v-list-item-content>
+                        <v-list-item-title class="white-text">{{link.text}}</v-list-item-title>
+                    </v-list-item-content>
+                    
+                </v-list-item>
+            </v-list>
+        </v-navigation-drawer>
+    </nav>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            drawer: false,
+            links: [
+                { icon: 'mdi-view-dashboard', text: 'Dashboard', route: '/' },
+                { icon: 'mdi-folder', text: 'My Projects', route: '/projects' },
+                { icon: 'mdi-account', text: 'Team', route: '/team' },
+            ]
+        }
+    }
+}
+</script>
---
+++ src/views/Home.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="home">
+    <h1>Home</h1>
+    <v-btn class="hidden-sm-only">hellow</v-btn>
+  </div>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
